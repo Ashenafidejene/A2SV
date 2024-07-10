06b921b3eee3ca6b565669358fb146fe4a68b75a
ASHENAFI DEJENE NEGASH <121222976+Ashenafidejene@users.noreply.github.com>
2024-07-10 19:01:43 +0300

    Time: 403 ms (78.95%) | Memory: 30 MB (69.03%) - LeetSync

--- 1330-longest-arithmetic-subsequence-of-given-difference/longest-arithmetic-subsequence-of-given-difference.py
+++ 1330-longest-arithmetic-subsequence-of-given-difference/longest-arithmetic-subsequence-of-given-difference.py
@@ -3,5 +3,14 @@ class Solution:
         dp = defaultdict(int)
         for num in arr:
             dp[num] = dp[num - difference] + 1
-
         return max(dp.values())
+
+
+          # dic [0:0 1 : 1 2:2 , 3 : 3 4:4] 
+                
+           
+
+
+
+        
+        
