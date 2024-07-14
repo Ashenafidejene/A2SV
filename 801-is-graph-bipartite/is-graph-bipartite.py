331cc40c4b6fdf8504b2327b5b983fd429dda6b4
ASHENAFI DEJENE NEGASH <121222976+Ashenafidejene@users.noreply.github.com>
2024-07-14 16:55:45 +0300

    Time: 139 ms (86.13%) | Memory: 17.2 MB (19.41%) - LeetSync

---
+++ 801-is-graph-bipartite/is-graph-bipartite.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isBipartite(self, graph: List[List[int]]) -> bool:
+        n = len(graph)
+        color = [-1] * n
+        result = True
+        
+        def dfs(node):
+            for neighbor in graph[node]:
+                if color[neighbor] == -1:
+                    color[neighbor] = 1 - color[node]
+                    if not dfs(neighbor):
+                        return False
+                elif color[neighbor] == color[node]:
+                    return False
+            return True
+        
+        for node in range(n):
+            if color[node] == -1:
+                color[node] = 0
+                if not dfs(node):
+                    return False
+        return True
+    
