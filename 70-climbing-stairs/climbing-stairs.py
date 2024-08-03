f1db885e4cafaae5ea79d912ca7b19d080c53d4c
ASHENAFI DEJENE NEGASH <121222976+Ashenafidejene@users.noreply.github.com>
2024-08-03 17:39:48 +0300

    Time: 30 ms (83.79%) | Memory: 16.6 MB (13.96%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,8 +1,13 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        array = [1,1]
-        for index in range(2,n+1):
-            array.append(array[index-1] + array[index-2])
-        return array[n]
+        memo = {}
+        def dp(k):
+            if k < 3:
+                return k 
+            if  k not in  memo :
+                memo[k] = dp(k-1) + dp(k-2)
+            return memo[k]
+        return dp(n)
+
 
         
