8bda6d16839c690c028f7805295ec4fca505a694
ASHENAFI DEJENE NEGASH <121222976+Ashenafidejene@users.noreply.github.com>
2024-08-08 08:49:46 +0300

    Time: 34 ms (97.70%) | Memory: 16.6 MB (15.87%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.py
+++ 12-integer-to-roman/integer-to-roman.py
@@ -1,35 +1,68 @@
 class Solution:
     def intToRoman(self, num: int) -> str:
-        roman_numerals = [
-            ("M", 1000),
-            ("CM", 900),
-            ("D", 500),
-            ("CD", 400),
-            ("C", 100),
-            ("XC", 90),
-            ("L", 50),
-            ("XL", 40),
-            ("X", 10),
-            ("IX", 9),
-            ("V", 5),
-            ("IV", 4),
-            ("I", 1)
-        ]
-        
-        result = []
-        for roman, value in roman_numerals:
-            while num >= value:
-                result.append(roman)
-                num -= value
-        
-        return ''.join(result)
-                 
-
-
-
-
-                
-
-
-                
-                
+        self.ans = ""
+        def changer(n):
+            if n == 0 :
+                return 
+            if n < 5 :
+                if n == 4 :
+                    self.ans +="IV"
+                else:
+                    val = ("I"*n)
+                    self.ans += val 
+            elif n < 10 :
+                if n%5 == 4 :
+                    self.ans +="IX"
+                    return 
+                else:
+                   n = n - 5 
+                   self.ans +="V"
+                   val = ("I"*n)
+                   self.ans += val 
+                return 
+            elif n < 50:
+                k = n % 10 
+                l = n//10
+                if l == 4:
+                    self.ans += "XL"
+                else:
+                    val = ("X" * l)
+                    self.ans += val 
+                changer(k)
+            elif n < 100 : 
+                k = n % 10
+                l = n // 10 
+                if l == 9 :
+                    self.ans += "XC"
+                else:
+                    self.ans +="L"
+                    val = ("X" * (l-5))
+                    self.ans += val 
+                changer(k)
+            elif n < 500 :
+               k = n % 100
+               l = n//100
+               if l == 4:
+                  self.ans += "CD"
+               else :
+                    val = ("C" * l)
+                    self.ans += val 
+               changer(k)
+            elif n < 1000:
+                k = n % 100
+                l = n//100
+                if l == 9:
+                    self.ans += "CM"
+                else:
+                    self.ans +="D"
+                    val = ("C" * (l-5))
+                    self.ans += val 
+                changer(k)
+            else:
+                k = n % 1000
+                l = n // 1000
+                val = ("M" * l)
+                self.ans += val 
+                changer(k)
+        changer(num)
+        return self.ans
