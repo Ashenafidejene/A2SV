f564347ef09187e2abef411918a7dd01b38e6a2d
ASHENAFI DEJENE NEGASH <121222976+Ashenafidejene@users.noreply.github.com>
2024-08-15 11:04:34 +0300

    Time: 94 ms (84.86%) | Memory: 16.6 MB (88.46%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.py
+++ 518-coin-change-ii/coin-change-ii.py
@@ -1,12 +1,8 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
-        coins.sort()
-        @lru_cache(None)
-        def dp(i, amounts):
-            if amounts == 0:
-                return 1
-            if amounts < 0 or i >= len(coins):
-                return 0
-            return dp(i, amounts - coins[i]) + dp(i + 1, amounts)
-        
-        return dp(0, amount)
+        dp = [0] *( amount+1)
+        dp[0]=1
+        for coin in coins :
+            for x in range(coin,amount+1):
+                dp[x] += dp[x-coin]
+        return dp[amount]
