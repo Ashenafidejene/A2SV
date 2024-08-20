228fcdeaf10294d7fb35a8a9e722ef80813e9a7d
ASHENAFI DEJENE NEGASH <121222976+Ashenafidejene@users.noreply.github.com>
2024-08-20 22:00:42 +0300

    Time: 304 ms (66.73%) | Memory: 18.3 MB (64.90%) - LeetSync

---
+++ 1240-stone-game-ii/stone-game-ii.py
@@ -0,0 +1,30 @@
+class Solution:
+    def stoneGameII(self, piles: List[int]) -> int:
+        n = len(piles)
+        
+        
+        suffix_sum = [0] * n
+        suffix_sum[-1] = piles[-1]
+        for i in range(n - 2, -1, -1):
+            suffix_sum[i] = suffix_sum[i + 1] + piles[i]
+        
+        
+        memo = {}
+        
+        def dp(i, M):
+            if i >= n:
+                return 0
+            if (i, M) in memo:
+                return memo[(i, M)]
+            
+            max_stones = 0
+            for X in range(1, 2 * M + 1):
+                if i + X <= n:
+                    max_stones = max(max_stones, suffix_sum[i] - dp(i + X, max(M, X)))
+            
+            memo[(i, M)] = max_stones
+            return max_stones
+        
+        return dp(0, 1)
+                                                                                                                  
+            
