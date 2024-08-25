dcc9fe2f821f7ae035d811b1fe74baa48d6ff4b0
ASHENAFI DEJENE NEGASH <121222976+Ashenafidejene@users.noreply.github.com>
2024-08-25 10:25:30 +0300

    Time: 84 ms (5.06%) | Memory: 26.3 MB (5.06%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
@@ -1,14 +1,17 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        @lru_cache
+        memo = {}
         def dp(i,flag):
             if i >= len(prices):
                 return 0
+            if (i,flag) in memo :
+                return memo[(i,flag)]
             value1 = dp(i+1,flag)
             value2 = 0 
             if flag :
                 value2 = -prices[i]+dp(i+1,False)
             else:
                 value2 = prices[i] + dp(i+1,True)
-            return max(value2,value1)
+            memo[(i,flag)]=max(value2,value1)
+            return memo[(i,flag)]
         return dp(0,True)
