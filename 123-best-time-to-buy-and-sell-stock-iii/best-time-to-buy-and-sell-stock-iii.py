9f71e7ef135e68893d2497685b62212c2c0ecb8e
ASHENAFI DEJENE NEGASH <121222976+Ashenafidejene@users.noreply.github.com>
2024-08-25 11:07:32 +0300

    Time: 2224 ms (10.31%) | Memory: 447.8 MB (11.11%) - LeetSync

---
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.py
@@ -0,0 +1,25 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        memo = {}
+        
+        def dp(i, noTrans, holding):
+            if i >= len(prices) or noTrans >= 2: 
+                return 0
+            
+            if (i, holding, noTrans) in memo:
+                return memo[(i, holding, noTrans)]
+            
+            
+            value = dp(i + 1, noTrans, holding)
+            
+            if holding:
+                
+                value2 = prices[i] + dp(i + 1, noTrans + 1, False)
+            else:
+               
+                value2 = -prices[i] + dp(i + 1, noTrans, True)
+            
+            memo[(i, holding, noTrans)] = max(value, value2)
+            return memo[(i, holding, noTrans)]
+        
+        return dp(0, 0, False)
