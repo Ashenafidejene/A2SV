dbafe8ab8247cbdfd62334616e60faf32c1a0bd5
ASHENAFI DEJENE NEGASH <121222976+Ashenafidejene@users.noreply.github.com>
2024-09-02 15:29:17 +0300

    Time: 243 ms (39.05%) | Memory: 0B (100.00%) - LeetSync

---
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -0,0 +1,19 @@
+-- Write your PostgreSQL query statement below
+with SingleValue As(
+    SELECT 
+    Employee.salary AS salary
+    from Employee
+    GROUP BY Employee.salary
+),
+RankEmployee AS(
+    SELECT 
+    SingleValue.salary As SecondHighestSalary,
+    DENSE_RANK() OVER (ORDER BY SingleValue.salary DESC) As rank 
+    from SingleValue
+)
+SELECT 
+    CASE 
+        WHEN EXISTS (SELECT 1 FROM RankEmployee WHERE rank = 2) 
+        THEN (SELECT SecondHighestSalary FROM RankEmployee WHERE rank = 2)
+        ELSE NULL 
+    END AS SecondHighestSalary;
