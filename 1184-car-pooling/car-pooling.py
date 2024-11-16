90372634613e36a5b4326441adfb1b10cf9abc6f
ASHENAFI DEJENE NEGASH <121222976+Ashenafidejene@users.noreply.github.com>
2024-11-16 17:46:51 +0300

    Time: 0 ms (100.00%) | Memory: 17.1 MB (34.39%) - LeetSync

--- 1184-car-pooling/car-pooling.py
+++ 1184-car-pooling/car-pooling.py
@@ -1,24 +1,13 @@
 class Solution:
     def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
-        # Find the maximum drop-off location to size the timeline array
         max_location = max(trip[2] for trip in trips)
-        
-        # Initialize the timeline array with zeros
-        timeline = [0] * (max_location + 1)
-        
-        # Update the timeline for each trip
-        for num_passengers, from_loc, to_loc in trips:
-            timeline[from_loc] += num_passengers
-            if to_loc < len(timeline):
-                timeline[to_loc] -= num_passengers
-        
-        # Calculate the prefix sum and check the capacity
-        current_passengers = 0
-        for passengers in timeline:
-            current_passengers += passengers
-            if current_passengers > capacity:
-                return False
-        
-        return True
-
-
+        prifixSum = [0]*(max_location+1)
+        for tripP in trips:
+            prifixSum[tripP[1]]+=tripP[0]
+            prifixSum[tripP[2]]-=tripP[0]
+        current_passagenr = 0
+        for i in range(len(prifixSum)) :
+                current_passagenr=current_passagenr+prifixSum[i]
+                if current_passagenr > capacity:
+                    return False
+        return True 
