ac59a1f68e51aeff6eae2ade79acad5c80f0d387
ASHENAFI DEJENE NEGASH <121222976+Ashenafidejene@users.noreply.github.com>
2024-12-15 11:28:27 +0300

    Time: 27 ms (85.20%) | Memory: 19.8 MB (33.30%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -1,17 +1,14 @@
 
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        prefixSum = {0: 1}  # Initialize a hashmap to store prefix sums and their frequencies
-        accumulator = 0
-        counter = 0
+        prifixSum = {0:1}
+        sums = 0
+        answer = 0
+        for num in nums :
+            sums +=num
+            if sums - k in prifixSum :
+                answer += prifixSum[sums-k]
+            prifixSum[sums]=1+prifixSum.get(sums,0)
+        return answer 
         
-        for num in nums:
-            accumulator += num
-            # Check if (accumulator - k) exists in the prefixSum hashmap
-            if accumulator - k in prefixSum:
-                # Increment the counter by the frequency of (accumulator - k)
-                counter += prefixSum[accumulator - k]
-            # Increment the frequency of accumulator in the prefixSum hashmap
-            prefixSum[accumulator] = prefixSum.get(accumulator, 0) + 1
-        
-        return counter
+       
