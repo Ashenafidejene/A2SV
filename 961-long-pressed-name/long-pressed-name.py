a0296d3870111bf2a8e4acb0c07c982d835ea32c
ASHENAFI DEJENE NEGASH <121222976+Ashenafidejene@users.noreply.github.com>
2024-12-17 17:00:33 +0300

    Time: 0 ms (100.00%) | Memory: 17.8 MB (8.56%) - LeetSync

--- 961-long-pressed-name/long-pressed-name.py
+++ 961-long-pressed-name/long-pressed-name.py
@@ -1,32 +1,23 @@
 class Solution:
     def isLongPressedName(self, name: str, typed: str) -> bool:
-        if len(name) > len( typed):
-            return False 
-        if len(name) == len( typed):
-            return name == typed 
-        if len(name) == 1:
-            for char in  typed:
-                if char != name[0]:
-                    return False 
-            return True 
-        n , t = 0,0 
-        while n < len(name)-1 or t < len( typed)-1:
-            if n < len(name)-1 and t < len( typed) -1 :
-                if name[n:n+2] == typed[t:t+2]:
-                    n+=1
-                    t+=1
-                elif typed[t] == typed[t+1]:
-                    t+=1
-                else :
-                    return False
-            elif t < len( typed) -1 and n >= len(name)-1:
-                if typed[t] == typed[t+1]:
-                    t+=1
-                else :
-                    return False
+        if len(name)>len(typed):
+            return False
+        if len(name)==len(typed):
+            return name == typed
+        f , s = 0,0 
+        while s < len(typed) and f < len(name):
+            if name[f]==typed[s]:
+                f+=1
+                s+=1
+            elif name[f]!=typed[s] and s >0 and typed[s] == typed[s-1]:
+                s+=1
             else:
                 return False
-        return True
-
+        if s == len(typed) and f <len(name):
+            return False
+        for k in range(s,len(typed)):
+            if typed[k]!=name[-1]:
+                return False
+        return True 
 
 
