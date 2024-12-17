0f41019597c56c04038b21bb5b6a474e0d678ad4
ASHENAFI DEJENE NEGASH <121222976+Ashenafidejene@users.noreply.github.com>
2024-12-17 16:02:09 +0300

    Time: 553 ms (71.06%) | Memory: 20.8 MB (13.71%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,27 +1,26 @@
 class Solution:
     def threeSum(self, nums):
         nums.sort()
-        n = len(nums)
-        res = []
-        
-        for i in range(n - 2):
+        length = len(nums)
+        answerset = set()
+        answer = []
+        for i in range(length -2):
             if i > 0 and nums[i] == nums[i - 1]:
                 continue
-            
-            left, right = i + 1, n - 1
-            while left < right:
-                total = nums[i] + nums[left] + nums[right]
-                if total < 0:
-                    left += 1
-                elif total > 0:
-                    right -= 1
+            b,e = i+1 , length-1
+            while b < e :
+                sums = nums[i]+nums[b]+nums[e]
+                if sums > 0:
+                    e-=1
+                elif sums < 0:
+                    b+=1
                 else:
-                    res.append([nums[i], nums[left], nums[right]])
-                    while left < right and nums[left] == nums[left + 1]:
-                        left += 1
-                    while left < right and nums[right] == nums[right - 1]:
-                        right -= 1
-                    left += 1
-                    right -= 1
-        
-        return res
+                    answer.append([nums[i],nums[b],nums[e]])
+                    b+=1
+                    e-=1
+                    while b < e and nums[b] == nums[b - 1]:
+                        b += 1
+                    while b < e and nums[e] == nums[e + 1]:
+                        e -= 1
+                
+        return answer 
