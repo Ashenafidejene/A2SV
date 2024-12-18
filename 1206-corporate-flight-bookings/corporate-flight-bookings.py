6e2f9ac2a4428ea2a6a2dfef1b478fcdb3f712a3
ASHENAFI DEJENE NEGASH <121222976+Ashenafidejene@users.noreply.github.com>
2024-12-18 12:51:39 +0300

    Time: 24 ms (66.79%) | Memory: 29.1 MB (9.49%) - LeetSync

--- 1206-corporate-flight-bookings/corporate-flight-bookings.py
+++ 1206-corporate-flight-bookings/corporate-flight-bookings.py
@@ -1,10 +1,12 @@
 class Solution:
     def corpFlightBookings(self, bookings: List[List[int]], n: int) -> List[int]:
-        num = [0] * ( n + 1)
-        for i in range(len(bookings)):
-            num[bookings[i][0]-1]+=bookings[i][2]
-            num[bookings[i][1]]-=bookings[i][2]
-        for j in range(1,len(num)):
-            num[j] = num[j]+num[j-1]
-
-        return num[0:n]
+        prifixSum = [0]*(n+1)
+        for order in bookings :
+            first,last,seat = order
+            prifixSum[first-1]+=seat
+            prifixSum[last]-=seat
+        acumulator = 0
+        for i in range(1,len(prifixSum)):
+            prifixSum[i]+=prifixSum[i-1]
+        return prifixSum[0:n]
+        
