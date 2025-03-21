a2e21bed3659552ba3f484635a031fd2ccb36be1
ASHENAFI DEJENE NEGASH <121222976+Ashenafidejene@users.noreply.github.com>
2025-03-21 17:58:17 +0300

    Time: 63 ms (30.60%) | Memory: 21.1 MB (19.94%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.py
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.py
@@ -4,16 +4,34 @@
 #         self.val = x
 #         self.left = None
 #         self.right = None
-
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        if root is None:
-            return None
+        stack1 = []
+        stack2 = []
+        def finder(root,stack,val):
+            if not root:
+                return stack
+            stack.append(root)
+            if root.val == val :
+                return stack
+            
+            if root.val > val:
+                return finder(root.left,stack[:],val)
+            else:
+                return finder(root.right,stack[:],val)
         
-        if root.val > p.val and root.val > q.val:
-            return self.lowestCommonAncestor(root.left, p, q)
+        stack1 = finder(root,[],p.val)
+        stack2 = finder(root,[],q.val)
+        while stack1 and stack2:
+            if stack1[-1] == stack2[-1]:
+                return stack1[-1]
+            if len(stack1) > len(stack2):
+                stack1.pop()
+            elif len(stack1) <len(stack2):
+                stack2.pop()
+            else:
+                stack1.pop()
+                stack2.pop()
+        return None
+
         
-        if root.val < p.val and root.val < q.val:
-            return self.lowestCommonAncestor(root.right, p, q)
-        
-        return root
