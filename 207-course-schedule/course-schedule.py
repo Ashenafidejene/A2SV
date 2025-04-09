c8b9ff1c0a6aa88a814e4355ad626054020d3ade
ASHENAFI DEJENE NEGASH <121222976+Ashenafidejene@users.noreply.github.com>
2025-04-09 18:51:21 +0300

    Time: 6 ms (54.13%) | Memory: 19.6 MB (22.19%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -1,21 +1,25 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        graph = {i:[] for i in range(numCourses)}
-        for crs , pre in prerequisites:
-            graph[crs].append(pre)
-        visit = set()
+        graph  = {i:[] for i in range(numCourses)}
+        for ai  , bi in prerequisites:
+            graph[ai].append(bi)
+        white = 1 
+        gray = 2 
+        black = 3 
+        color = {i : white for i in range(numCourses)}
         def dfs(node):
-            if node in visit:
-                return False 
-            if graph[node] == []:
-                return True 
-            visit.add(node)
-            for pre in graph[node]:
-                if not dfs(pre):
-                    return False 
-            visit.remove(node)
-            graph[node]=[]
+            if color[node] == 2 :
+                return False
+            if color[node] == 1 :
+                color[node] = 2 
+                for nigbour in graph[node]:
+                   if not  dfs(nigbour):
+                      return False
+                color[node] = 3
             return True 
-        for crs in range(numCourses):
-            if not dfs(crs):return False
+        for i in range(numCourses):
+            #print(i,color)
+            if not dfs(i):
+
+                return False
         return True 
